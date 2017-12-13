831dba64f2a227173e20cafd99ec17c873774117
steven <751836594@qq.com>
2017-12-13 17:37:30 +0800

    login

--- src/main/view.py
+++ src/main/view.py
@@ -49,8 +49,7 @@ def auth_qq():
         get_access_token_url = 'https://graph.qq.com/oauth2.0/token?grant_type=authorization_code&client_id=%s&client_secret=%s&code=%s&redirect_uri=%s' % (
             auth_list['APPID'], auth_list['APPKey'], code, auth_list['REDIRECTURI'])
         resp = urllib.request.urlopen(get_access_token_url).read()
-        return resp[13:32]
-        access_token = resp.split('&')[0].split('=')[1]
+        access_token = resp[13:32]
 
         get_open_id_url = "https://graph.qq.com/oauth2.0/me?access_token=%s" % (access_token)
 
