e9039523725fc93cb4e259bcf13683d99c0f5fb3
steven <751836594@qq.com>
2017-12-13 17:45:27 +0800

    login

--- src/main/view.py
+++ src/main/view.py
@@ -50,7 +50,7 @@ def auth_qq():
             auth_list['APPID'], auth_list['APPKey'], code, auth_list['REDIRECTURI'])
         resp = urllib.request.urlopen(get_access_token_url)
         access_token = resp.read()[13:45]
-        resp.close()
+        return access_token
         get_open_id_url = "https://graph.qq.com/oauth2.0/me?access_token=%s" % (access_token)
 
         res = urllib.request.urlopen(get_open_id_url).read()
